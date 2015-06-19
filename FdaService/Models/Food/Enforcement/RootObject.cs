3aa474280f8e09418cd8bf0811307be8818893ef
msneen <michael_sneen@yahoo.com>
2015-06-19 16:46:41 -0700

    Code Cleanup

--- FdaService/Models/Food/Enforcement/RootObject.cs
+++ FdaService/Models/Food/Enforcement/RootObject.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 namespace FdaService.Models.Food.Enforcement
 {
     public class RootObject
-{
-    public Meta meta { get; set; }
-    public List<Result> results { get; set; }
-}
+    {
+        public Meta meta { get; set; }
+        public List<Result> results { get; set; }
+    }
 }
