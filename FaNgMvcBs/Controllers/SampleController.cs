f4d3d8a647e67b18bfcfd08bd191e4502d4cd2b3
msneen <michael_sneen@yahoo.com>
2015-06-23 16:47:58 -0700

    Added Aaron's Web Project to the solution. Modified the project to Reference the web Service.

---
+++ FaNgMvcBs/Controllers/SampleController.cs
@@ -0,0 +1,70 @@
+﻿using FaNgMvcBs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebGrease.Css.Extensions;
+
+namespace FaNgMvcBs.Controllers
+{
+    public class SampleController : ApiController
+    {
+        public List<SampleModel> Get()
+        {
+            var session = HttpContext.Current.Session;
+            if (session["Data"] == null)
+            {
+                session["Data"] = SampleModel.BuildModels();
+            }
+            return (List<SampleModel>)session["Data"];
+        }
+
+        public SampleModel Get(int id)
+        {
+            var session = HttpContext.Current.Session;
+            var result = (List<SampleModel>)session["Data"];
+            return result.FirstOrDefault(d => d.Id == id);
+        }
+
+        public object Post(SampleModel item)
+        {
+            var session = HttpContext.Current.Session;
+            var result = (List<SampleModel>)session["Data"];
+            var success = false;
+            try
+            {
+                item.Created = DateTime.UtcNow;
+                item.Id = result.Max(r => r.Id) + 1;
+                result.Add(item);
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+            return new { Successful = success };
+        }
+
+        public object Delete(int id)
+        {
+            var session = HttpContext.Current.Session;
+            var result = (List<SampleModel>)session["Data"];
+            var success = false;
+            try
+            {
+                result.Where(r => r.ParentId == id).ForEach(r =>
+                {
+                    r.ParentId = 0;
+                });
+                result.Remove(result.FirstOrDefault(d => d.Id == id));
+                success = true;
+            }
+            catch
+            {
+                success = false;
+            }
+            return new { Successful = success };
+        }
+    }
+}
