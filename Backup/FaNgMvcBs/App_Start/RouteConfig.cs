e19c61411ca8b1313e4f043eb9e532b05702624f
msneen <michael_sneen@yahoo.com>
2015-07-01 12:41:50 -0700

    Upgraded to 4.6 .Net

---
+++ Backup/FaNgMvcBs/App_Start/RouteConfig.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FaNgMvcBs
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            //To assess session from Api. 
+            //routes.MapRoute(
+            //	name: "DefaultApi",
+            //	url: "api/{controller}/{id}",
+            //	defaults: new {id = RouteParameter.Optional}
+            //);
+
+            //apiRoute.RouteHandler = new ApiContollerRouteHandler();
+        }
+    }
+}
---
+++ Backup/FaNgMvcBs/Global.asax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.WebHost;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace FaNgMvcBs
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        //TODO: Remove, this preserves state and makes session available to API. To make the API restful again. Remove the block below.
+        protected void Application_PostAuthorizeRequest()
+        {
+            var routeData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(HttpContext.Current));
+            if (routeData != null && routeData.RouteHandler is HttpControllerRouteHandler)
+                HttpContext.Current.SetSessionStateBehavior(SessionStateBehavior.Required);
+        }
+    }
+}
